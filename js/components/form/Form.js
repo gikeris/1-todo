e52042f1a695e0bae07a34c793d10d9b55a39a9d
gikeris <gintautas.kondrasevicius@gmail.com>
2021-08-24 00:10:57 +0300

    form input validation

--- js/components/form/Form.js
+++ js/components/form/Form.js
@@ -3,6 +3,11 @@ class Form{
         this.selector = selector;
 
         this.DOM = null;
+        this.messageDOM = null;
+        this.colorDOM = null;
+        this.dateDOM = null;
+        this.completeDOM = null;
+        this.saVeButtonDOM = null;
     }
     init(){
         if(!this.isValidSelector()
@@ -12,6 +17,7 @@ class Form{
          }
          
          this.render();
+         this.addEvents();
     }
 
 
@@ -53,11 +59,50 @@ class Form{
         </div>
     </form>`;
 
-this.DOM.insertAdjacentHTML('afterbegin', HTML);
+        this.DOM.insertAdjacentHTML('afterbegin', HTML);
+        
+        this.messageDOM = this.DOM.querySelector('textarea');
+        this.colorDOM = this.DOM.querySelector('input[type="color"]');
+        this.dateDOM = this.DOM.querySelector('input[type="date"]');
+        this.completeDOM = this.DOM.querySelector('input[type="checkbox"]');
+        this.saveButtonDOM = this.DOM.querySelector('button.save');
+    }
 
+    addEvents() {
+        this.saveButtonDOM.addEventListener('click', (e) =>{
+            e.preventDefault();
+            console.log('click zzz');
+            const message = this.messageDOM.value;
+            const color = this.colorDOM.value;
+            const date = this.dateDOM.value;
+            const completed = this.completeDOM.checked;
 
+            if (this.isValidTask(message, color, date, completed)){
+                console.log('task is valid...');
+            }
+    });
+    }
+    isValidTask(){
+        if(!isValidMessage(message)||
+            !isValidColor(color)||
+            !isValidDate(date)||
+            !isValidCompleted(completed)){
+            return false;
+        }
+        return true;
+    }
+    isValidMessage(message){
+        return true;
+    }
+    isValidColor(color){
+        return true;
+    }
+    isValidDate(date){
+        return true;
+    }
+    isValidCompleted(completed){
+        return true;
     }
 }
-
 
 export { Form };
